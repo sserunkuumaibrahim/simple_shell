9ffce951f84e49e8e5b1a62bc95c81949eae9861
sserunkuuma ibrahim <sserunkuumaibrahim@gmail.com>
2023-02-22 01:43:31 -0800

    simple shell group project

--- main.c
+++ main.c
@@ -1,13 +1,45 @@
-#include "main.h"
+#include "shell.h"
 
 /**
  * main - entry point
- * Return: always 0 success
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(void)
+int main(int ac, char **av)
 {
-	char x = 55;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	write(1, &x, 1);
-	return (0);
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
+
